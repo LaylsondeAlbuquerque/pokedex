1b9bc54d52b9c60b84b3e9aef0f618e8bc7a116c
LaylsondeAlbuquerque <laylsondealbuquerque@gmail.com>
2025-10-31 10:21:15 -0300

    configurações iniciais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { PokemonList } from './pages/pokemon-list/pokemon-list';
+import { PokemonDetail } from './pages/pokemon-detail/pokemon-detail';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/list',
+        pathMatch: 'full'
+    },
+    {
+        path: 'list',
+        component: PokemonList
+    },
+    {
+        path: 'detail/:name',
+        component: PokemonDetail
+    }
+];
