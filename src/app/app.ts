5d3f5e711b77d735c35352b21667d807351fbca3
LaylsondeAlbuquerque <laylsondealbuquerque@gmail.com>
2025-10-31 18:06:11 -0300

    Criação da página de carregamento

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import { LoadingService } from './services/loading-service';
+import { Loading } from './pages/loading/loading';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, Loading],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('pokedex');
+
+  public loadingService = inject(LoadingService);
 }
--- src/app/pages/pokemon-list/pokemon-list.ts
+++ src/app/pages/pokemon-list/pokemon-list.ts
@@ -1,24 +1,34 @@
+// Imports
 import { Component, OnInit, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { Pokemon } from '../../services/pokemon';
 import { PokemonResult } from '../../models/pokemon';
+import { LoadingService } from '../../services/loading-service';
 
-
+// Component Decorator
 @Component({
   selector: 'app-pokemon-list',
   imports: [RouterLink],
   templateUrl: './pokemon-list.html',
   styleUrl: './pokemon-list.css',
 })
+
+// Component Class
 export class PokemonList {
-  private pokemonService = inject(Pokemon)
+  // Injects
+  private pokemonService = inject(Pokemon);
+  private isLoadingService = inject(LoadingService);
 
+  // Signals
   pokemonList = signal<PokemonResult[]>([]);
-  isLoading = signal<boolean>(true);
+  isLoading = this.isLoadingService.isLoading;
 
+  // Lifecycle Hook
   ngOnInit() {
     console.log('Componente foi iniciado. Buscando Dados...');
+
+    this.isLoading.set(true);
 
     this.pokemonService.getPokemonList()
       .subscribe({
@@ -26,7 +36,9 @@ export class PokemonList {
         next: (dataApi) => {
           console.log('Dados recebidos da API:', dataApi);
           this.pokemonList.set(dataApi.results);
-          this.isLoading.set(false);
+          setTimeout(() => {
+            this.isLoading.set(false);
+          }, 2000);
         },
 
         error: (err) => {
